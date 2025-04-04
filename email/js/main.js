7272476230de05c821f778d82e371bad55be37cb
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-04 14:30:32 +0200

    email

---
+++ email/js/main.js
@@ -0,0 +1,25 @@
+// Crea una lista di email di invitati ad una festa. 
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, 
+// stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+//  Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’. 
+//  Nota: Non è necessario provvedere alla validazione delle email
+
+const invitedEmail =[
+    'utente1@gmail.com',
+    'utente2@gmail.com',
+    'utente3@gmail.com',
+    'utente4@gmail.com',
+    'utente5@gmail.com',
+    'utente6@gmail.com',
+    'utente7@gmail.com',
+    'utente8@gmail.com',
+    'utente9@gmail.com'
+];
+const userEmail = prompt('inserisci la tua email:');
+let emailFound = false;
+for(let i = 0; i < invitedEmail.length; i++){
+    if(userEmail === invitedEmail[i]){
+        emailFound = true;
+    }
+}
+emailFound? console.log('email trovata, puoi entrare') : console.log('email non trovata, non puoi accedere');
