5514719fb2733d66cd8e4128690ba1a052f6916d
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-04 14:40:31 +0200

    dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -2,3 +2,17 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 //  Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+const playerNumber = Math.floor((Math.random() * 6) + 1);
+const computerNumber = Math.floor((Math.random() * 6) + 1);
+
+console.log('player number: '+ playerNumber);
+console.log('computer number: '+ computerNumber);
+
+if(playerNumber > computerNumber){
+    console.log('il giocatore ha vinto');
+}else if (playerNumber < computerNumber){
+    console.log('il computer ha vinto');
+}else{
+    console.log('pareggio');
+}
